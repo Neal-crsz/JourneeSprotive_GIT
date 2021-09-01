1f7be869cfdf0614a0c3ba695613805dd3014c83
Neal <neal.crsz@eduge.ch>
2021-09-01 10:51:19 +0200

    Chapitre 1 Création formulaire
    Fin le chapitre 1 : création formulaire en html et php pour la journée sportive

---
+++ htmlToPhp.inc.php/afficherSelectActivites.php
@@ -0,0 +1,40 @@
+
+                    <td><label>Classe</label></td>
+                    <td>
+                    <select name="classe">
+                        <option value="classe1">IFA-P3A</option>
+                        <option value="classe2">IFA-P3B</option>
+                        <option value="classe3">IFA-P3C</option>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label>Premier Choix</label></td>
+                    <td>
+                    <select name="choix1">
+                        <option value="baseball">baseball</option>
+                        <option value="football">football</option>
+                        <option value="velo">velo</option>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label>Deuxieme choix</label></td>
+                    <td>
+                    <select name="choix2">
+                        <option value="baseball">baseball</option>
+                        <option value="football">football</option>
+                        <option value="velo">velo</option>
+                    </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label>troisieme choix</label></td>
+                    <td>
+                    <select name="choix3">
+                        <option value="baseball">baseball</option>
+                        <option value="football">football</option>
+                        <option value="velo">velo</option>
+                    </select>
+                    </td>
+                </tr>
---
+++ inscription.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Journée Sportive du CFPT</title>
+    </head>
+    <body>
+        <?php
+        $nom = filter_input(INPUT_POST, 'Nom');
+        $prenom = filter_input(INPUT_POST, 'Prenom');
+
+        // quand le formulaire est confirmer
+        if (filter_input(INPUT_POST, 'envoyer') == true)
+        {
+        if(isset($_POST['classe'])){
+            $classe = $_POST['classe'];
+        }
+        if(isset($_POST['choix1'])){
+            $choix1 = $_POST['choix1'];
+        }
+
+        if(isset($_POST['choix2'])){
+            $choix2 = $_POST['choix2'];
+        }
+
+        if(isset($_POST['choix3'])){
+            $choix3 = $_POST['choix3'];
+        }
+
+        //Affichage
+            echo 'Nom : ' . $nom . '<br>';
+            echo 'Prénom : ' . $prenom . '<br>';
+            echo 'classe : ' . $classe . '<br>';
+            echo 'choix1 : ' . $choix1 . '<br>';
+            echo 'choix2 : ' . $choix2 . '<br>';
+            echo 'choix3 : ' . $choix3 . '<br>';
+        }
+
+
+        ?>
+        <header>
+            Inscription à la journée sportive du CFPT
+        </header>
+        <form action="#" method="POST">
+            <table>
+                <tr>
+                    <td><label>Nom</label></td>
+                    <td><input type="text" name="Nom" id="Nom" value="<?= $nom ?>" require/></td>
+                </tr>
+                <tr>
+                    <td><label>Prénom</label></td>
+                    <td><input type="text" name="Prenom" id="Prenom" value="<?= $prenom ?>" require/></td>
+                </tr>
+                <tr>
+                    <?php
+                    require "htmlToPhp.inc.php/afficherSelectActivites.php";
+                    ?>
+                <tr>
+                    <td><input type="submit" name="envoyer" value="Confirmer"/></td>
+                </tr>
+            </table>
+        </form>
+        <footer>
+            CRAUSAZ NEAL
+        </footer>
+    </body>
+</html>
